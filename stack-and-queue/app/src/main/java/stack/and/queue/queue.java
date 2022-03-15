372e918c44c9f442780a16420924bb02a93629b6
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-03-15 22:59:32 +0300

    solved

---
+++ stack-and-queue/app/src/main/java/stack/and/queue/queue.java
@@ -0,0 +1,53 @@
+package stack.and.queue;
+
+public class queue<T> {
+    private Node<T> front;
+    private Node<T> back;
+
+    public void enqueue(T value){
+        Node<T> new_node = new Node<>();
+        new_node.value = (T) value;
+
+        if(this.front == null){
+            this.back = new_node;
+            this.front = new_node;
+            return;
+        }
+
+        this.back.next = new_node;
+        this.back = new_node;
+    }
+
+    public T dequeue(){
+        if(this.back == null){
+            throw new NullPointerException("it,s empty queue");
+        }
+
+        Node<T> temp = this.front;
+        this.front = temp.next;
+        temp.next = null;
+
+        return temp.value;
+    }
+
+    public T peek(){
+        return this.front.value;
+    }
+
+    public boolean isEmpty(){
+        return this.front == null;
+    }
+
+
+    public String toString(){
+        return setToString(this.front);
+    }
+
+    private String setToString(Node<T> node){
+        if (node == null){
+            return "null";
+        }
+
+        return node.value + "-->" + setToString(node.next);
+    }
+}
---
+++ stack-and-queue/app/src/main/java/stack/and/queue/stack.java
@@ -0,0 +1,45 @@
+package stack.and.queue;
+
+public class stack<T> {
+
+    private Node<T> top;
+
+    public void push( T value){
+        Node<T> new_node = new Node<>();
+        new_node.value = value;
+        new_node.next = this.top;
+        this.top = new_node;
+    }
+
+    public T pop(){
+
+      if(this.top == null) {
+       throw new NullPointerException("it,s empty stack.");
+       }
+        Node<T> temp = this.top;
+        this.top = temp.next;
+        temp.next = null;
+
+        return temp.value;
+    }
+
+    public T peek(){
+        return this.top.value;
+    }
+
+    public boolean isEmpty(){
+        return this.top == null;
+    }
+
+    public String toString(){
+        return setToString(this.top);
+    }
+
+    private String setToString(Node<T> node){
+        if (node == null){
+            return "null";
+        }
+
+        return node.value + "-->" + setToString(node.next);
+    }
+}
