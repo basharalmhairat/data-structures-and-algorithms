82f9c1cdb46be4513d5c16d91d147a964f74d46d
basharalmhairat <basharalmhairat6y6u@gmail.com>
2022-03-17 00:10:55 +0300

    solved

--- stack-and-queue/app/src/main/java/stack/and/queue/Main.java
+++ stack-and-queue/app/src/main/java/stack/and/queue/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         stack<Integer> stack = new stack<>();
-//        stack.push(3);
-//        stack.push(2);
-//        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
         stack.pop();
-//        System.out.println(stack.peek());
-//        System.out.println(stack);
-//        System.out.println(stack.isEmpty());
+        System.out.println(stack.peek());
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
         System.out.println("----------------------------------");
         queue<Integer>queue=new queue<>();
         queue.enqueue(3);
@@ -19,8 +19,15 @@ public class Main {
         queue.enqueue(1);
         System.out.println(queue.peek());
         queue.dequeue();
-
         System.out.println(queue.isEmpty());
         System.out.println(queue);
+        System.out.println("----------------------------------");
+stackQueuePseudo<Integer>Pseudo=new stackQueuePseudo<>();
+Pseudo.enqueue(3);
+Pseudo.enqueue(2);
+Pseudo.enqueue(1);
+        Pseudo.dequeue();
+
+        System.out.println(Pseudo.back);
     }
 }
